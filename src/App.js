81a5fa836344da1bfe00f8212b5427e006dea10d
sarvesh <47808172+sarveshjha07@users.noreply.github.com>
2020-09-20 15:59:14 +0530

    changes abc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import "./styles.css";
+function App() {
+  //new Date(year,month,day,hours,minutes,seconds,miiliseconds)
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let greeting = "";
+  const cssStyle = {};
 
-export default function App() {
+  if (curDate > 1 && curDate < 12) {
+    greeting = "Good morning";
+    cssStyle.color = "#00b894";
+  } else if (curDate >= 12 && curDate < 4) {
+    greeting = "Good Afternoon";
+    cssStyle.color = "orange";
+  } else if (curDate >= 4 && curDate < 20) {
+    greeting = "Good evning";
+    cssStyle.color = "gray";
+  } else {
+    greeting = "Good night";
+    cssStyle.color = "black";
+  }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div>
+        <h1>
+          Hello sir,<span style={cssStyle}> {greeting}</span>
+        </h1>
+      </div>
+    </>
   );
 }
+export default App;
